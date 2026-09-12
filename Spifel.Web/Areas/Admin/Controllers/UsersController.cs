b2d45b9df5a6ddfdb9b55b933dfb60b5a405e996
Kiyan Fahimy <kiyanfahimy@gmail.com>
2026-09-12 15:59:30 +0330

    Fix : fix admin layout Add users index view

---
+++ Spifel.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Spifel.Web.Areas.Admin.Controllers
+{
+    public class UsersController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
